91e8c4b74cd76e7d166afbbba7f9fc0eb6169347
Hans <ich.arbeite.rd>
2023-10-13 13:03:23 -0300

    correcion cliente unique

--- database/migrations/2023_10_13_141526_create_clientes_table.php
+++ database/migrations/2023_10_13_141526_create_clientes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->primary('id_usuario_cliente');
             $table->string('nombre_cliente');
             $table->enum('tipo_cliente', ['fisico', 'juridico']);
-            $table->unique('dni_cuit');
+            $table->string('dni_cuit')->unique();
             $table->string('codigo_postal_cliente');
             $table->timestamps();
             $table->unsignedBigInteger('id_usuario_cliente');
