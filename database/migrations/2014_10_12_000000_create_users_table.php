05223211b578a2db979110b0cab842f0d1c24824
Hans <ich.arbeite.rd>
2023-10-13 11:58:04 -0300

    k

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->enum('rol_usuario', ['fisico, juridico']);
+            $table->enum('rol_usuario', ['cliente, jefe_ventas', 'administrador', 'gerente']);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
