108925b1cf1c958e8b9961060a5a75889d7479b0
Hans <ich.arbeite.rd>
2023-10-13 11:24:16 -0300

    k

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Venta extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["nombre_cliente", "tipo_cliente", "dni_cuit", "codigo_postal_cliente", "email"]; //campos solicitados al momento de enviar el request
+    protected $table = "clientes"; //tabla a referenciar
+
+    //Relaciones 
+
+    //M a 1 Clientes
+    
 }
