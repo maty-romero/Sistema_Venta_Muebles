ba06e3fed771fc9e6c946eb1071641d1cc70d790
JavArm94 <javarm94@gmail.com>
2023-10-13 11:02:03 -0300

    Model commit

---
+++ app/Models/Cliente.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [""]; //campos solicitados al momento de enviar el request
+    protected $table = "clienstes"; //tabla a referenciar
+
+    // Relaciones 
+
+}
---
+++ app/Models/ProductoOferta.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductoOferta extends Model
+{
+    use HasFactory;
+}
