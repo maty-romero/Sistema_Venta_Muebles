d379ebf40955553ed61e03e42cc7407d19eb4b18
Hans <ich.arbeite.rd>
2023-10-18 00:03:17 -0300

    componentes tailwind (inicio)

---
+++ resources/views/cliente/usuario/index.blade.php
@@ -0,0 +1,1 @@
+{{-- Pefil del usuario --}}
---
+++ resources/views/components/custom/card.blade.php
@@ -0,0 +1,13 @@
+<!-- component -->
+<div class="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+    <img class="h-48 w-full object-cover object-center" src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Product Image" />
+    <div class="p-4">
+      <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">Product Name</h2>
+      <p class="mb-2 text-base dark:text-gray-300 text-gray-700">Product description goes here.</p>
+      <div class="flex items-center">
+        <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">$20.00</p>
+        <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
+        <p class="ml-auto text-base font-medium text-green-500">20% off</p>
+      </div>
+    </div>
+  </div>
