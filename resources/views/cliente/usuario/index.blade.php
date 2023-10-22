4d0cad9c148b12d284af42cff3d7133407b55ff0
Hans <ich.arbeite.rd>
2023-10-22 18:38:51 -0300

    vista Index Perfil cliente

--- resources/views/cliente/usuario/index.blade.php
+++ resources/views/cliente/usuario/index.blade.php
@@ -17,41 +17,56 @@
     <title>Perfil Cliente</title>
 
 </head>
-<body class="antialiased bg-[#FFE794] shadow-[0_2px_4px_-0px_rgba(0,0,0,0.25)] bg-pattern-image ">
-    <div id="infoCliente" class="container mx-auto bg-white rounded-2xl p-6">
-        <h3>Usuario</h3>
-        <form action="">
-            <div class="grid place-items-center">
-            <table>
-                <tr>
-                    <td><label>Nombre</label></td>
-                    <td><label>Personer&iacute;a</label></td>
-                    <td><label>Documento</label></td>         
-                </tr>
-                <tr >
-                    <td ><input id="txtNombre" type="text" ></td>
-                    <td><input id="txtPersoneria" type="text" disabled></td>
-                    <td><input id="txtDocumento" type="text"></td>
-                </tr>
-                <tr>
-                    <td class="pt-8"><label>Email</label></td>
-                    <td class="pt-8"><label>C&oacute;digo Postal</label></td>
-                    <td class="pt-8"><label>N&uacute;mero de T&eacute;lefono</label></td>         
-                </tr>
-                <tr>
-                    <td><input id="txtEmail" type="text"></td>
-                    <td><input id="txtCodigoPostal" type="text"></td>
-                    <td><input id="txtNroTel" type="text"></td>
-                </tr>
-            </table>
-            </div>
-            <button id="btnCambioContrasenia" type="button">Editar Contraseña</button>
-        </form>
-        <button id="btnConfirmarCambios" type="button">Confirmar cambios</button>
-    </div>
+<body class="antialiased bg-[#FFE794] shadow-[0_2px_4px_-0px_rgba(0,0,0,0.25)] bg-pattern-image">
+    <x-custom.navbar>
+    </x-custom.navbar>
     <br><br>
+    <div id="infoCliente" class="container mx-auto bg-white rounded-2xl p-6">
+        <h3 class="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">Usuario</h3>
+        <form action="">
+
+            <div class="py-5">
+                <table>
+                    <tr>
+                        <td class="pr-16"><label class="font-poppins">Nombre</label></td>
+                        <td class="pr-16"><label class="font-poppins">Personer&iacute;a</label></td>
+                        <td class="pr-16"><label class="font-poppins">Documento</label></td>
+                    </tr>
+                    <tr>
+                        <td class="pr-16"><input id="txtNombre" type="text" class="rounded-md"></td>
+                        <td class="pr-16"><input id="txtPersoneria" type="text" disabled class="rounded-md"></td>
+                        <td class="pr-16"><input id="txtDocumento" type="text" class="rounded-md"></td>
+                    </tr>
+                    <tr>
+                        <td class="pr-16 pt-8"><label class="font-poppins">Email</label></td>
+                        <td class="pr-16 pt-8"><label class="font-poppins">C&oacute;digo Postal</label></td>
+                        <td class="pr-16 pt-8"><label class="font-poppins">N&uacute;mero de T&eacute;lefono</label></td>
+                    </tr>
+                    <tr>
+                        <td class="pr-16"><input id="txtEmail" type="text" class="rounded-md"></td>
+                        <td class="pr-16"><input id="txtCodigoPostal" type="text" class="rounded-md"></td>
+                        <td class="pr-16"><input id="txtNroTel" type="text" class="rounded-md"></td>
+                    </tr>
+                </table>
+            </div>
+            
+            <div class="relative flex flex-row">
+                <button class="flex select-none items-center gap-2 rounded-lg py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase bg-gray-500 text-white hover:bg-gray-500 transition-all active:bg-gray-600 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+                    Editar Contraseña
+                </button>
+            </div>
+            <br>
+            <div class="relative flex flex-row">
+                <button id="btnConfirmarCambios" class="ml-auto py-3 px-6 rounded-lg text-center font-sans text-sm font-bold uppercase bg-green-500 text-white hover:bg-green-600 transition-all" type="button">
+                    Confirmar cambios
+                </button>
+            </div>
+            
+        </form>
+    </div>
+    
     <div id="comprasRecientes" class="container mx-auto mt-10 bg-white rounded-2xl p-6">
-        <h2 class="">Compras recientes</h2><br>
+        <h2 class="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">Compras recientes</h2><br>
         <div>
             <x-custom.sale-item>
             </x-custom.sale-item>
