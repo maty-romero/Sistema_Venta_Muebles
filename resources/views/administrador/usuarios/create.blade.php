98e876c394e0d413925a4d02baa27aeadbae1d01
Hans <ich.arbeite.rd>
2023-10-28 15:55:01 -0300

    update Crear usuario view - using layout

--- resources/views/administrador/usuarios/create.blade.php
+++ resources/views/administrador/usuarios/create.blade.php
@@ -1,60 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
+@extends('layouts.administrativo')
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+@section('titulo')
+    Crear usuario
+@endsection
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@section('encabezado')
+    Crear usuario
+@endsection
 
-    <title>Crear Usuario</title>
+@section('contenido')
+<div id="detalleVenta" class="container mx-auto p-6">
+    <form action="">
+        <p class="font-poppins text-1g">Nombre Usuario</p>
+        <input id="txtNombreUsuario" type="text" class="rounded-md mb-10">
 
-</head>
-<body class="antialiased bg-[#F0F0F0] bg-pattern-image">
-    <x-custom.navbar_adm>
-    </x-custom.navbar_adm>
-    <div id="detalleVenta" class="container mx-auto p-6">
-        <h1 class="mb-2 block font-sans text-5xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">Crear Usuario</h1><br>
-        
-        <form action="">
-            <p class="font-poppins text-1g">Nombre Usuario</p>
-            <input id="txtNombreUsuario" type="text" class="rounded-md mb-10">
+        <p class="font-poppins text-1g">Email</p>
+        <input id="txtEmail" type="email" class="rounded-md mb-10">
 
-            <p class="font-poppins text-1g">Email</p>
-            <input id="txtEmail" type="email" class="rounded-md mb-10">
+        <p class="font-poppins text-1g">Rol de Usuario</p>
 
-            <p class="font-poppins text-1g">Rol de Usuario</p>
-
-            <select name="cmbRolUsuario" class="font-poppins text-1g">
-                <option value="cliente">Cliente</option>
-                <option value="jefe_ventas" selected>Jefe de Ventas</option>
-                <option value="gerente">Gerente</option>
-            </select>
-        
-            <br>
-            <button class="mt-7 bg-gray-800 text-white py-2 px-4 rounded-md text-base">
-                Confirmar
-            </button>
-        </form>
-        
-        
-    </div>
+        <select name="cmbRolUsuario" class="font-poppins text-1g rounded-md">
+            <option value="cliente">Cliente</option>
+            <option value="jefe_ventas" selected>Jefe de Ventas</option>
+            <option value="gerente">Gerente</option>
+        </select>
+    
+        <br>
+        <button class="mt-7 bg-gray-800 text-white py-2 px-4 rounded-md text-base">
+            Confirmar
+        </button>
+    </form>
     
     
-    
-    
-    
-    
-    
-    
-              
-        
-         
-       
-</body>
-</html>
+</div>
+@endsection
