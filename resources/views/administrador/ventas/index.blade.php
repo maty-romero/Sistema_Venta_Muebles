bd320725e399bef752f4980c75169193651e5e43
Hans <ich.arbeite.rd>
2023-11-05 14:50:50 -0300

    paginacion index ventas

--- resources/views/administrador/ventas/index.blade.php
+++ resources/views/administrador/ventas/index.blade.php
@@ -9,6 +9,7 @@
 @endsection
 
 @section('contenido')
+
     <h3 class='text-3xl text-left ml-4'>Ordenar</h3>
     <div class="flex justify-between ml-4">
     <x-custom.filters>
@@ -25,28 +26,32 @@
     </x-custom.filters>
     </div>
 
-    <div class="w-full">
-    <x-custom.table :columnas="['Cliente', 'Personaria', 'Fecha', 'Total', 'Domicilio de Destino']">
-        @foreach ($ventas as $venta)
-            <tr>
-                <td
-                    class="px-5 py-3 border-b-2 border-gray-500 bg-slate-100 text-left text-lg font-semibold text-gray-900">
-                    {{ $venta['nombre_cliente'] }}</td>
-                <td
-                    class="px-5 py-3 border-b-2 border-gray-500 bg-slate-100 text-left text-lg font-semibold text-gray-900">
-                    {{ $venta['tipo_cliente'] }}</td>
-                <td
-                    class="px-5 py-3 border-b-2 border-gray-500 bg-slate-100 text-left text-lg font-semibold text-gray-900">
-                    {{ date('d-m-Y', strtotime($venta['fecha_venta'])) }}</td>
-                <td
-                    class="px-5 py-3 border-b-2 border-gray-500 bg-slate-100 text-left text-lg font-semibold text-gray-900">
-                    @money($venta['total_venta'])</td>
-                <td
-                    class="px-5 py-3 border-b-2 border-gray-500 bg-slate-100 text-left text-lg font-semibold text-gray-900">
-                    {{ $venta['domicilio_venta'] }}</td>
-                
-            </tr>
-        @endforeach
-    </x-custom.table>
+    <div class="w-full text-center">
+        <x-custom.table :columnas="['Cliente', 'Personaria', 'Fecha', 'Total', 'Domicilio de Destino']">
+            @foreach ($ventas as $venta)
+                <tr>
+                    <td
+                        class="px-5 py-3 border-b-2 border-gray-500 bg-slate-100 text-left text-lg font-semibold text-gray-900">
+                        {{ $venta->cliente->nombre_cliente }}</td>
+                    <td
+                        class="px-5 py-3 border-b-2 border-gray-500 bg-slate-100 text-left text-lg font-semibold text-gray-900">
+                        {{ $venta->cliente->tipo_cliente }}</td>
+                    <td
+                        class="px-5 py-3 border-b-2 border-gray-500 bg-slate-100 text-left text-lg font-semibold text-gray-900">
+                        {{ date('d-m-Y', strtotime($venta->fecha_venta)) }}</td>
+                    <td
+                        class="px-5 py-3 border-b-2 border-gray-500 bg-slate-100 text-left text-lg font-semibold text-gray-900">
+                        @money($venta->monto_final_venta)</td>
+                    <td
+                        class="px-5 py-3 border-b-2 border-gray-500 bg-slate-100 text-left text-lg font-semibold text-gray-900">
+                        {{ $venta->domicilio_destino }}</td>
+                </tr>
+            @endforeach
+        </x-custom.table>
+    
+        <div class="flex justify-center">{{ $ventas->links() }}</div>
     </div>
+    
+
+
 @endsection
