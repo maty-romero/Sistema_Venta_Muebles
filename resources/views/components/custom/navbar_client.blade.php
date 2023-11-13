6727eb88d13c4250ef0a11a8d6f102ddb8641c0b
Zair02 <110054488+Zair02@users.noreply.github.com>
2023-11-13 13:09:59 -0300

    Corrección navbar al buscar

--- resources/views/components/custom/navbar_client.blade.php
+++ resources/views/components/custom/navbar_client.blade.php
@@ -3,6 +3,11 @@
     <div class="container w-full py-3 md:flex md:justify-between md:items-center">
         @if (isset($name))
         <div class="flex justify-between items-center">
+            <div class="flex justify-center items-center ml-auto pr-4">
+                <a class="relative text-gray-500 font-bold" href="{{route('home')}}">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M20 9v11a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9"/><path d="M9 22V12h6v10M2 10.6L12 2l10 8.6"/></svg>
+                </a>
+            </div>
             <x-custom.input-search :name="$name">
                 </x-input-search>
         </div>
