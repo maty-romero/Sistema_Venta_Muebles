6e46327c639deb8776ad2f56f66a3f4c02c866f1
Zair02 <110054488+Zair02@users.noreply.github.com>
2023-11-13 23:33:23 -0300

    Fix

--- app/Rules/OfertaComboValida.php
+++ app/Rules/OfertaComboValida.php
@@ -15,27 +15,28 @@ class OfertaComboValida implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $tipoOferta = request()->input('fechaInicio');
-        $fechaInicio = request()->input('fechaInicio');
-        $fechaFin = request()->input('fechaFin');
+        if(request()->input('tipoOferta') == 'combo'){
+            $fechaInicio = request()->input('fechaInicio');
+            $fechaFin = request()->input('fechaFin');
 
-        $comboSimilar = true;
-        foreach($value as $prod){
-            $idProd = (int)explode(".", $prod)[0];
-            $cant = (int)explode("x", $prod)[count(explode("x", $prod))-1];
+            $comboSimilar = true;
+            foreach($value as $prod){
+                $idProd = (int)explode(".", $prod)[0];
+                $cant = (int)explode("x", $prod)[count(explode("x", $prod))-1];
 
-            $results = DB::select(
-            "SELECT o.id, o.fecha_inicio_oferta, o.fecha_fin_oferta
-            FROM `ofertas` AS o
-            INNER JOIN `oferta_combo_producto` AS cp ON o.id = cp.id_oferta_combo
-            WHERE cp.id_producto = '$idProd' AND cp.cantidad_producto_combo = '$cant'
-                AND (('$fechaInicio' BETWEEN o.fecha_inicio_oferta AND o.fecha_fin_oferta) 
-                OR ('$fechaFin' BETWEEN o.fecha_inicio_oferta AND o.fecha_fin_oferta)
-                OR (o.fecha_inicio_oferta BETWEEN '$fechaInicio' AND '$fechaFin')
-                OR (o.fecha_fin_oferta BETWEEN '$fechaInicio' AND '$fechaFin'))");
-            
-            if(count($results) <= 0){
-                $comboSimilar = false;
+                $results = DB::select(
+                "SELECT o.id, o.fecha_inicio_oferta, o.fecha_fin_oferta
+                FROM `ofertas` AS o
+                INNER JOIN `oferta_combo_producto` AS cp ON o.id = cp.id_oferta_combo
+                WHERE cp.id_producto = '$idProd' AND cp.cantidad_producto_combo = '$cant'
+                    AND (('$fechaInicio' BETWEEN o.fecha_inicio_oferta AND o.fecha_fin_oferta) 
+                    OR ('$fechaFin' BETWEEN o.fecha_inicio_oferta AND o.fecha_fin_oferta)
+                    OR (o.fecha_inicio_oferta BETWEEN '$fechaInicio' AND '$fechaFin')
+                    OR (o.fecha_fin_oferta BETWEEN '$fechaInicio' AND '$fechaFin'))");
+                
+                if(count($results) <= 0){
+                    $comboSimilar = false;
+                }
             }
         }
 
--- app/Rules/OfertaUnitariaValida.php
+++ app/Rules/OfertaUnitariaValida.php
@@ -19,26 +19,28 @@ class OfertaUnitariaValida implements ValidationRule
 
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $fechaInicio = request()->input('fechaInicio');
-        $fechaFin = request()->input('fechaFin');
-        foreach($value as $prod){
-            $idProd = (int)explode(".", $prod)[0];
+        if(request()->input('tipoOferta') == 'unitaria'){
+            $fechaInicio = request()->input('fechaInicio');
+            $fechaFin = request()->input('fechaFin');
+            foreach($value as $prod){
+                $idProd = (int)explode(".", $prod)[0];
 
-            $results = DB::select(
-                "SELECT p.id, o.id 
-                FROM `productos` AS p 
-                INNER JOIN `oferta_producto` AS op ON p.id = op.id_producto
-                INNER JOIN `ofertas` AS o ON o.id = op.id_oferta
-                WHERE p.id = '$idProd'
-                    AND (('$fechaInicio' BETWEEN o.fecha_inicio_oferta AND o.fecha_fin_oferta) 
-                    OR ('$fechaFin' BETWEEN o.fecha_inicio_oferta AND o.fecha_fin_oferta)
-                    OR (o.fecha_inicio_oferta BETWEEN '$fechaInicio' AND '$fechaFin')
-                    OR (o.fecha_fin_oferta BETWEEN '$fechaInicio' AND '$fechaFin'))");
+                $results = DB::select(
+                    "SELECT p.id, o.id 
+                    FROM `productos` AS p 
+                    INNER JOIN `oferta_producto` AS op ON p.id = op.id_producto
+                    INNER JOIN `ofertas` AS o ON o.id = op.id_oferta
+                    WHERE p.id = '$idProd'
+                        AND (('$fechaInicio' BETWEEN o.fecha_inicio_oferta AND o.fecha_fin_oferta) 
+                        OR ('$fechaFin' BETWEEN o.fecha_inicio_oferta AND o.fecha_fin_oferta)
+                        OR (o.fecha_inicio_oferta BETWEEN '$fechaInicio' AND '$fechaFin')
+                        OR (o.fecha_fin_oferta BETWEEN '$fechaInicio' AND '$fechaFin'))");
 
-            if(count($results) > 0){
-                $fail = 'Uno de los productos ya tiene una oferta de Unitaria dentro de ese periodo';
+                if(count($results) > 0){
+                    $fail = 'Uno de los productos ya tiene una oferta de Unitaria dentro de ese periodo';
+                }
             }
+            $fail = 'ProbandoUnitaria';
         }
-        $fail = 'ProbandoUnitaria';
     }
 }
