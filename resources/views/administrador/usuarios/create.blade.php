520a93a464b4e5e6635d278c078e51edf16d5ec5
Hans <ich.arbeite.rd>
2023-11-13 23:05:08 -0300

    formulario crear Usuario

--- resources/views/administrador/usuarios/create.blade.php
+++ resources/views/administrador/usuarios/create.blade.php
@@ -12,19 +12,42 @@
 <div id="detalleVenta" class="container mx-auto p-6">
     <form action="">
         <p class="font-poppins text-1g">Nombre Usuario</p>
-        <input id="txtNombreUsuario" type="text" class="rounded-md mb-10">
+        <input id="txtNombreUsuario" name="nombreUsuario" type="text" class="rounded-md mb-5">
 
         <p class="font-poppins text-1g">Email</p>
-        <input id="txtEmail" type="email" class="rounded-md mb-10">
+        <input id="txtEmail" name="email" type="email" class="rounded-md mb-5">
+
+        <p class="font-poppins text-1g">Contraseña</p>
+        <input id="txtContrasenia" name="password" type="password" class="rounded-md mb-5">
 
         <p class="font-poppins text-1g">Rol de Usuario</p>
-
-        <select name="cmbRolUsuario" class="font-poppins text-1g rounded-md">
+        <select id="idcCbRolUsuario" name="cmbRolUsuario" class="font-poppins text-1g rounded-md mb-5">
             <option value="cliente">Cliente</option>
             <option value="jefe_ventas" selected>Jefe de Ventas</option>
             <option value="gerente">Gerente</option>
         </select>
     
+        <div id="camposCliente">
+            <p class="font-poppins text-1g">Nombre cliente</p>
+            <input id="txtNombreCliente" name="nombreCliente" type="text" class="rounded-md mb-5"> 
+            
+            <p class="font-poppins text-1g">Tipo de cliente</p>
+            <select id="idCmbTipoCliente" name="cmbTipoCliente" class="font-poppins text-1g rounded-md mb-5">
+                <option value="fisico">Fisico</option>
+                <option value="juridico" selected>Juridico</option>
+            </select>
+
+            <p class="font-poppins text-1g">DNI / CUIT</p>
+            <input id="txtDniCuit" name="dni_cuit" type="number" class="rounded-md mb-5 ">
+
+            <p class="font-poppins text-1g">Codigo postal</p>
+            <input id="txtCodigoPostal" name="codigoPostal" type="number" class="rounded-md mb-5">
+
+            <p class="font-poppins text-1g">N&uacute;mero de T&eacute;lefono</p>
+            <input id="txtCodigoPostal" name="codigoPostal" type="number" class="rounded-md mb-5">
+        </div>
+
+
         <br>
         <button class="mt-7 bg-gray-800 text-white py-2 px-4 rounded-md text-base">
             Confirmar
