c37e237af976e8afb926a8dfc3a3923d24b55cd7
Zair02 <110054488+Zair02@users.noreply.github.com>
2024-02-14 03:56:54 -0300

    Validacion Oferta monto

--- app/Models/OfertaMonto.php
+++ app/Models/OfertaMonto.php
@@ -50,6 +50,25 @@ class OfertaMonto extends Model
     }
 
     public static function validarOfertaMonto(){
+        $fechaInicio = request()->input('fechaInicio');
+        $fechaFin = request()->input('fechaFin');
+        $monto = request()->input('montoMin');
 
+        $results = DB::select(
+            "SELECT o.id
+        FROM `ofertas_montos` AS om
+        INNER JOIN `ofertas` AS o ON o.id = om.id_oferta_monto
+        WHERE om.monto_limite_descuento = '$monto'
+        AND (o.deleted_at IS NULL and om.deleted_at IS NULL)
+            AND (('$fechaInicio' BETWEEN o.fecha_inicio_oferta AND o.fecha_fin_oferta) 
+            OR ('$fechaFin' BETWEEN o.fecha_inicio_oferta AND o.fecha_fin_oferta)
+            OR (o.fecha_inicio_oferta BETWEEN '$fechaInicio' AND '$fechaFin')
+            OR (o.fecha_fin_oferta BETWEEN '$fechaInicio' AND '$fechaFin'))"
+        );
+
+        if (count($results) > 0) {
+            return false;
+        }
+        return true;
     }
 }
