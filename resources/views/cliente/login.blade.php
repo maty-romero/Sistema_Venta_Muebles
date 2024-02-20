4ae78b651ab0f65fc7e1d72330842e60f246686b
Zair02 <110054488+Zair02@users.noreply.github.com>
2024-02-20 02:12:51 -0300

    Fix
    Se eliminó del modal de login:
     - Recordarme
     - Recuperacion de contraseña

--- resources/views/cliente/login.blade.php
+++ resources/views/cliente/login.blade.php
@@ -48,25 +48,16 @@
                     <input id='password' name='password' type='password' required class='block w-full mt-1 border-gray-400 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm'>
                 </div>
                 <div class="flex items-center justify-end mt-4">
-                    <label for="remember_me" class="inline-flex items-center">
-                        <input id="remember_me" type="checkbox" class="rounded border-gray-400 text-zinc-700 shadow-sm focus:ring-indigo-500" name="remember">
-                        <span class="ml-2 text-sm text-gray-600">{{ __('Recordarme') }}</span>
-                    </label>
-                    <button type='submit' class='inline-flex ml-auto items-center px-6 py-2 bg-zinc-700 hover:bg-zinc-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150'>Iniciar
-                        sesi&oacute;n</button>
-                </div>
-                <div class='flex pt-4 mt-4 border-t border-t-zinc-200'>
-                    @if (Route::has('password.request'))
-                    <a class="underline mr-2 text-sm text-zinc-700 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        Olvidaste tu contrase&ntilde;a?
-                    </a>
-                    @endif
+                    <button type='submit' class='inline-flex mr-auto items-center px-6 py-2 bg-zinc-700 hover:bg-zinc-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150'>Iniciar
+                        sesi&oacute;n
+                    </button>
                     <p class='text-right ml-auto text-sm text-zinc-500 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
                         <a class="underline text-sm text-zinc-700 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
                             Registrarse
                         </a>
                     </p>
                 </div>
+                
             </form>
         </div>
     </div>
