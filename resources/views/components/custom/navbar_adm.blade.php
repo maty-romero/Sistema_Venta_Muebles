f6b0fb897cc48446e19f4336c2087d814298bcf1
Hans <ich.arbeite.rd>
2024-03-17 18:46:34 -0300

    Fix nombre cerrar sesion nav

--- resources/views/components/custom/navbar_adm.blade.php
+++ resources/views/components/custom/navbar_adm.blade.php
@@ -14,7 +14,7 @@
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
                         <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
-                            {{ __('Log Out') }}
+                            {{ __('Cerrar sesion') }}
                         </x-dropdown-link>
                     </form>
                     @else
